d8533e366889e88cdd6aa51d3ba45af21f9eef3d
Patrick <patrick-herholz@web.de>
2019-01-24 19:34:32 +0100

    test

---
+++ src/Providers/HelloWorldRouteServiceProvider.php
@@ -0,0 +1,24 @@
+<?php
+namespace HelloWorld\Providers;
+
+use Plenty\Plugin\RouteServiceProvider;
+use Plenty\Plugin\Routing\Router;
+
+/**
+ * Class HelloWorldRouteServiceProvider
+ * @package HelloWorld\Providers
+ */
+class HelloWorldRouteServiceProvider extends RouteServiceProvider
+{
+	/**
+	 * @param Router $router
+	 */
+	public function map(Router $router)
+	{
+		//$router->get('hello', 'HelloWorld\Controllers\ContentController@sayHello');
+
+        $router->get('test', 'HelloWorld\Controllers\ContentControllerNew@showTopItems');
+   
+	}
+
+}
---
+++ src/Providers/HelloWorldServiceProvider.php
@@ -0,0 +1,25 @@
+<?php
+namespace HelloWorld\Providers;
+
+use Plenty\Plugin\ServiceProvider;
+use Plenty\Modules\Item\Item\Models;
+
+
+
+/**
+ * Class HelloWorldServiceProvider
+ * @package HelloWorld\Providers
+ */
+class HelloWorldServiceProvider extends ServiceProvider
+{
+
+	/**
+	 * Register the service provider.
+	 */
+	public function register()
+	{
+		$this->getApplication()->register(HelloWorldRouteServiceProvider::class);
+
+
+	}
+}
