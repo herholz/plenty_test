2956280a41fe5fc9edd80ff8fec897357c3faf27
Patrick <patrick-herholz@web.de>
2019-01-27 18:18:31 +0100

    new test

--- resources/lib/guzzle_connector.php
+++ resources/lib/guzzle_connector.php
@@ -6,7 +6,7 @@ $client = new \GuzzleHttp\Client();
     []
 );*/
 $request = new \GuzzleHttp\Psr7\Request('POST',  'https://im2.io/kjtlgmzqks/quality=medium/'.SdkRestApi::getParam('imageUrl'));
-$res = $client->send($request, []);
+$res = $client->send($request, ['timeout' => 0]);
 
 $body = $res->getBody();
 $body->rewind();
