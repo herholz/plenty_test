5344a31074212a7261dd6aa190b5dc95cd1adad9
Patrick <patrick-herholz@web.de>
2019-03-11 14:38:40 +0100

    test

--- src/Providers/HelloWorldRouteServiceProvider.php
+++ src/Providers/HelloWorldRouteServiceProvider.php
@@ -13,13 +13,16 @@ class HelloWorldRouteServiceProvider extends RouteServiceProvider
 	/**
 	 * @param Router $router
 	 */
-	public function map(Router $router)
+	/*public function map(Router $router)
 	{
 		$router->get('hello', 'HelloWorld\Controllers\ContentController@sayHello');
         $router->get('test', 'HelloWorld\Controllers\ContentControllerNew@showTopItems');
         $router->get('pivot', 'HelloWorld\Controllers\ContentControllerNew@showPivot');
+	}*/
 
-   
-	}
+	public function register()
+        {
+          
+        }
 
 }
