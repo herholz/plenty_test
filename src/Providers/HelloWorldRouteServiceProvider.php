a2a9b7f99540025d691df5b7cbfe8d06a21d88ed
Patrick <patrick-herholz@web.de>
2019-03-11 16:05:59 +0100

    test twig

--- src/Providers/HelloWorldRouteServiceProvider.php
+++ src/Providers/HelloWorldRouteServiceProvider.php
@@ -14,20 +14,20 @@ class HelloWorldRouteServiceProvider extends RouteServiceProvider
 	/**
 	 * @param Router $router
 	 */
-	/*public function map(Router $router)
+	public function map(Router $router)
 	{
 		$router->get('hello', 'PivotPlugin\Controllers\ContentController@sayHello');
         $router->get('test', 'PivotPlugin\Controllers\ContentControllerNew@showTopItems');
         $router->get('pivot', 'PivotPlugin\Controllers\ContentControllerNew@showPivot');
-	}*/
+	}
 
-    public function map(ApiRouter $api)
+    /*public function map(ApiRouter $api)
     {
         $api->version(['v1'], ['namespace' => 'PivotPlugin\Controllers', 'middleware' => ['oauth']], function ($api)
         {
             $api->get('/pivot', 'ContentControllerNew@showPivot');
         });
-    }
+    }*/
 
 	public function register()
         {
